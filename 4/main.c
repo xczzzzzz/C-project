70c72a1c55f68300d4ebac118c5be1ea7ddf0e47
Chenxi Zhu <707380155@qq.com>
2021-03-26 20:03:06 +0800

    快速排序

--- 4/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-void qsort(int v[],int left,int right);
-main(){
-    int v[] = {3,4,2,1,4,5,10,9,7};
-    printf(v);
-    qsort(v,0,sizeof v)
-    return 0;
-}
--- 5/main.c
+++ 5/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-
-int main() {
-    printf("Hello, World!\n");
+void qsort(int v[],int left,int right);
+int main(){
+    int v[] = {3,4,2,1,4,5,10,9,7};
+    qsort(v,0,9);
     return 0;
 }
