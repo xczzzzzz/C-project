14bd5bfd2a4664fd8bfdb9bc1963a10dac7e7e10
Chenxi Zhu <707380155@qq.com>
2021-03-29 15:25:02 +0800

    修改bug

--- 5/qsort.c
+++ 5/qsort.c
@@ -10,7 +10,7 @@ void qsort(int v[],int left,int right){
     swap(v,left,(left+right)/2);
     last = left;
     for(i = left +1 ;i<=right;i++)
-        if (v[i] < v[]left)
+        if (v[i] < v[left]);
             swap(v,++last,i);
     swap(v,left,last);
     qsort(v,left,last-1);
