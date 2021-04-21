2d8f63df1fae641cf2c90b57022a5dc62dbfb934
Chenxi Zhu <707380155@qq.com>
2021-04-21 19:59:50 +0800

    malloc 返回void型的指针地址，在使用时需要强行转换一次

---
+++ struct_project/cmake-build-debug/CMakeFiles/struct_project.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/struct_project.dir/getch.c.obj"
+  "CMakeFiles/struct_project.dir/main.c.obj"
+  "libstruct_project.dll.a"
+  "struct_project.exe"
+  "struct_project.exe.manifest"
+  "struct_project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/struct_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
